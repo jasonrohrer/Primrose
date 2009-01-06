ebc624b6489fd222047da216bd0db90251ca0230
jcr13 <jcr13@a8a40712-a08a-474a-b844-89d20b818029>
2009-01-06 16:43:03 +0000

    Got mouse and screen projection working.

--- trunk/gameSource/game.cpp
+++ trunk/gameSource/game.cpp
@@ -1,19 +1,51 @@
 #include "game.h"
 
+#include <stdio.h>
 
-void initFrameDrawer( int inWidth, int inHeight ) {}
+#include <GL/gl.h>
+
+
+int pointerX, pointerY;
+
+
+void initFrameDrawer( int inWidth, int inHeight ) {
+    pointerX = -100;
+    pointerY = -100;
+    }
 
 void freeFrameDrawer() {}
 
 
 
-void drawFrame() {}
+void drawFrame() {
+    glColor4d( 1.0, 1.0, 1.0, 1.0 );
+    glBegin( GL_LINES ); {
+        
+        glVertex2d( 0, 0 );
+        glVertex2d( pointerX, pointerY );
+        }
+    glEnd();
+    
+
+
+    }
 
 
 
-void pointerDownPoint( float inX, float inY ) {}
+void pointerDown( float inX, float inY ) {
+    pointerX = inX;
+    pointerY = inY;
+    }
 
 
-void pointerMovePoint( float inX, float inY ) {}
+void pointerMove( float inX, float inY ) {
+    pointerX = inX;
+    pointerY = inY;
+    printf( "Pointer %d,%d\n", pointerX, pointerY );
+    
+    }
 
-void pointerUpPoint( float inX, float inY ) {}
+void pointerUp( float inX, float inY ) {
+    pointerX = inX;
+    pointerY = inY;
+    }
