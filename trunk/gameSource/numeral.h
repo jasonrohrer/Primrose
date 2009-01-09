1f4a3528249af1049ae3f949fd7b315d0367a3e0
jcr13 <jcr13@a8a40712-a08a-474a-b844-89d20b818029>
2009-01-09 19:07:14 +0000

    Added code for drawing numerals.

---
+++ trunk/gameSource/numeral.h
@@ -0,0 +1,9 @@
+
+
+#include "minorGems/graphics/Color.h"
+
+
+// 10 for comma
+void drawNumeral( int inNumber,
+                  float inCenterX, float inCenterY,
+                  Color *inColor = NULL, float inAlpha = 1.0 );
