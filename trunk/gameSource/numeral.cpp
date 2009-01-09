f2ce215138f95400a294d8e84921771bf3d076ea
jcr13 <jcr13@a8a40712-a08a-474a-b844-89d20b818029>
2009-01-09 20:25:57 +0000

    Got score pip drawing working.

--- trunk/gameSource/numeral.cpp
+++ trunk/gameSource/numeral.cpp
@@ -4,6 +4,9 @@
 
 #include <GL/gl.h>
 
+#include <math.h>
+
+
 
 void drawNumeral( int inNumber,
                   float inCenterX, float inCenterY,
@@ -22,4 +25,108 @@ void drawNumeral( int inNumber,
 
     glDisable( GL_BLEND );
     }
+
+
+
+void drawScorePip( int inScore,
+                   float inCenterX, float inCenterY,
+                   Color *inColor, float inAlpha ) {
+    
+    int numDigits = (int)( floor( log10( inScore ) ) ) + 1;
+    
+
+    // draw up to 9 digits
+
+    if( numDigits > 9 ) {
+        printf( "Error:  more than 9 digits passed to drawScorePip: %d\n",
+                inScore );
+        
+        numDigits = 9;
+        }
+    
+    int rowSep = 2;
+    int colSep = 3;
+    
+    int centerOffsetsX[9] = { 
+        6 + colSep,
+        0,
+        -( 6 + colSep ),
+        6 + colSep,
+        0,
+        -( 6 + colSep ),
+        6 + colSep,
+        0,
+        -( 6 + colSep ) 
+        };
+    
+    int centerOffsetsY[9] = {
+        10 + rowSep,
+        10 + rowSep,
+        10 + rowSep,
+        0,
+        0,
+        0,
+        -( 10 + rowSep ),
+        -( 10 + rowSep ),
+        -( 10 + rowSep ) 
+        };
+    
+    // find center of mass for all digets together
+
+    int xSum = 0;
+    int ySum = 0;
+    
+    int i;
+    
+    for( i=0; i<numDigits; i++ ) {
+        xSum += centerOffsetsX[ i ];
+        ySum += centerOffsetsY[ i ];
+        }
+    
+    int cX = xSum / numDigits;
+    int cY = ySum / numDigits;
+
+
+    if( numDigits > 3 ) {
+        // center of mass doesn't look good for more than one row
+
+        cX = 0;
+        
+        if( numDigits < 7 ) {
+            cY = ( 10 + rowSep ) / 2;
+            }
+        else {
+            cY = 0;
+            }
+        }
+    
+    
+    
+    for( i=0; i<numDigits; i++ ) {
+        
+        int number = ( inScore / ( (int)( pow( 10, i ) ) ) ) % 10;
+        
+
+        drawNumeral( number,
+                     inCenterX - cX + centerOffsetsX[i], 
+                     inCenterY - cY + centerOffsetsY[i],
+                     inColor, inAlpha );
+        }
+
+    // commas
+    if( numDigits > 3 ) {
+        drawNumeral( 10,
+                     inCenterX - cX + centerOffsetsX[3] + 3 + colSep,
+                     inCenterY - cY + centerOffsetsY[3] + 1,
+                     inColor, inAlpha );
+        }
+    if( numDigits > 6 ) {
+        drawNumeral( 10,
+                     inCenterX - cX + centerOffsetsX[6] + 3 + colSep,
+                     inCenterY - cY + centerOffsetsY[6] + 1,
+                     inColor, inAlpha );
+        }
+    
+    }
+
 
--- trunk/gameSource/numeral.h
+++ trunk/gameSource/numeral.h
@@ -7,3 +7,9 @@
 void drawNumeral( int inNumber,
                   float inCenterX, float inCenterY,
                   Color *inColor = NULL, float inAlpha = 1.0 );
+
+
+
+void drawScorePip( int inScore,
+                   float inCenterX, float inCenterY,
+                   Color *inColor = NULL, float inAlpha = 1.0 );
