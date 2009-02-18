30a1d820143bfad47bacd627a5a7d1141dc141f9
jcr13 <jcr13@a8a40712-a08a-474a-b844-89d20b818029>
2009-02-18 19:13:04 +0000

    Website done?

--- trunk/documentation/html/index.php
+++ trunk/documentation/html/index.php
@@ -3,16 +3,27 @@
 
 
 <center>
-(FIXME:  points to Passage)<br>
-<a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=300702040&mt=8"<img src="storeBadge.png" width=174 height=58 border=0></a>
+<!--(FIXME:  points to Passage)<br>-->
+<a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=300702040&mt=8"><img src="storeBadge.png" width=174 height=58 border=0></a>
 </center>
 
-Description
+<br>
+<br>
+<br>
 
-Screenshots
+<font size=5><a href="description.php">Description</a></font>
 
-Mac/Win/Linux Downloads
+<br>
+<br>
+<br>
 
-Coming soon to iPhone, Mac, Windows, and GNU/Linux.
+<font size=5><a href="screens.php">Screenshots</a></font>
 
-<!--#include virtual="/foot.html" -->
+<br>
+<br>
+<br>
+
+<font size=5><a href="download.php">Downloads</a></font><br>
+Mac, Windows, and GNU/Linux
+
+ <?php  include_once( "foot.html" ); ?>
