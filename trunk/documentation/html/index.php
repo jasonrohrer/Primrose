387480ae173f7af9ee5e1c6f5af7df4f271727c0
jcr13 <jcr13@a8a40712-a08a-474a-b844-89d20b818029>
2009-02-19 13:30:22 +0000

    Fixed App Store link.

--- trunk/documentation/html/index.php
+++ trunk/documentation/html/index.php
@@ -4,7 +4,7 @@
 
 <center>
 <!--(FIXME:  points to Passage)<br>-->
-<a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=300702040&mt=8"><img src="storeBadge.png" width=174 height=58 border=0></a>
+<a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=302872239&mt=8"><img src="storeBadge.png" width=174 height=58 border=0></a>
 </center>
 
 <br>
