8caa1155608496587b1b21e1cc93358c25b4045f
nobody <nobody@a8a40712-a08a-474a-b844-89d20b818029>
2009-03-31 16:08:10 +0000

    This commit was manufactured by cvs2svn to create tag 'Primrose_v4'.

---
+++ tags/Primrose_v4/documentation/html/download.php
@@ -0,0 +1,29 @@
+
+<?php  include_once( "head.html" ); ?>
+
+
+<b>Note:</b>  These links point to the SourceForge download pages.<br>
+<br>
+<br>
+
+<ul>
+<li><a href="http://downloads.sourceforge.net/primrose/Primrose_v3_Windows.exe">Primrose_v3_Windows.exe</a>
+<br><br>
+
+<li><a href="http://downloads.sourceforge.net/primrose/Primrose_v3_MacOSX.dmg">Primrose_v3_MacOSX.dmg</a>
+<br><br>
+
+<li><a href="http://downloads.sourceforge.net/primrose/Primrose_v3_UnixSource.tar.gz">Primrose_v3_UnixSource.tar.gz</a>
+</ul>
+<br>
+<br>
+<br>
+
+To build from source on Mac/Win/Linux, you must have the <a href="http://libsdl.org">SDL</a> development library installed.<br>
+<br>
+The iPhone source code is included in the UnixSource distribution.  The iPhone code does not use SDL.<br>
+<br>
+<br>
+Primrose has been placed in the public domain.
+
+<?php  include_once( "foot.html" ); ?>
---
+++ tags/Primrose_v4/documentation/html/index.php
@@ -0,0 +1,34 @@
+
+<?php  include_once( "head.html" ); ?>
+
+
+<center>
+<a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=302872239&mt=8"><img src="storeBadge.png" width=174 height=58 border=0></a>
+</center>
+
+<br>
+<br>
+<br>
+
+<font size=5><a href="description.php">Description</a></font>
+
+<br>
+<br>
+<br>
+
+<font size=5><a href="screens.php">Screenshots</a></font>
+
+<br>
+<br>
+<br>
+
+<font size=5><a href="download.php">Downloads</a></font><br>
+Mac, Windows, and GNU/Linux
+
+<br>
+<br>
+<br>
+Version 3 rebalances the score formula, improving the game, and moves everyone to a new high score server. It also adds a FAST button for game replays.
+
+
+<?php  include_once( "foot.html" ); ?>
