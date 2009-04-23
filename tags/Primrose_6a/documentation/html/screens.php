a577772cf88e3d0c5fef326512812e67ea9b7944
nobody <nobody@a8a40712-a08a-474a-b844-89d20b818029>
2009-04-23 17:02:45 +0000

    This commit was manufactured by cvs2svn to create tag 'Primrose_6a'.

---
+++ tags/Primrose_6a/documentation/html/screens.php
@@ -0,0 +1,30 @@
+
+<?php  include_once( "head.html" ); ?>
+
+<center>
+<table border=0 cellspacing=20>
+
+<tr><td>
+<a href="screen.png"><img src="screen.png" width=80 height=120 border=1></a>
+</td>
+<td>
+<a href="screen1.png"><img src="screen1.png" width=80 height=120 border=1></a>
+</td>
+<td>
+<a href="screen2.png"><img src="screen2.png" width=80 height=120 border=1></a>
+</td>
+</tr>
+
+<tr>
+<td>
+<a href="screen3.png"><img src="screen3.png" width=80 height=120 border=1></a>
+</td>
+<td colspan=2>
+<a href="screen4.png"><img src="screen4.png" width=80 height=120 border=1></a>
+</td>
+</tr>
+
+</table>
+</center>
+
+<?php  include_once( "foot.html" ); ?>
