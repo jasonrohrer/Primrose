3ff619ae6d6ea649b02e1f502d7859ae1b45639c
nobody <nobody@a8a40712-a08a-474a-b844-89d20b818029>
2009-04-28 17:24:15 +0000

    This commit was manufactured by cvs2svn to create tag 'Primrose_v6c'.

---
+++ tags/Primrose_v6c/documentation/html/description.php
@@ -0,0 +1,61 @@
+
+<?php  include_once( "head.html" ); ?>
+
+<font size=5>From the App Store description:</font><br>
+<br>
+
+"A stand-out puzzler... it definitely has the stuff to become a new puzzle classic." --<a href="http://www.iphonegamenetwork.com/primrose-review">iPhone Games Network</a><br>
+<br>
+
+"A surprisingly deep and varied puzzle game that's well-worth your time." --<a href="http://jayisgames.com/archives/2009/03/mobile_monday_8.php">JayIsGames</a><br>
+<br>
+<br>
+
+
+Primrose is a captivating tile-clearing puzzle game by award-winning independent designer <a href="http://hcsoftware.sf.net/jason-rohrer">Jason Rohrer</a>.<br>
+<br>
+If you're bored by the crowd of copycat match-3 games...<br>
+<br>
+If you're looking for an elegant, deep puzzle game that's completely fresh...<br>
+<br>
+Then you should take a look at Primrose, which you can test for free (full version) on Mac, Windows, and GNU/Linux <a href="download.php">here</a>.<br>
+<br>
+Primrose's gameplay is deceptively simple:  You're given random pairs of colored tiles that you must place on a 7x7 grid.  When you surround a group of one color with another color, the surrounded group clears, scoring points.  The surrounding tiles flip to the color of the tiles that were cleared.  When tiles flip color, chain reactions are possible.  Larger groups and longer chain reactions are awarded more points.<br>
+<br>
+As the game progresses, more colors are added to the pool, making the grid more and more constrained.  Pressure builds until the grid finally fills up, and the game ends.<br>
+<br>
+Despite this mounting pressure, the game doesn't have a frantic feel, because you have as much time as you want to make each move.  Whereas games like Tetris depend on getting faster and faster to be challenging, Primrose derives deep challenge from inherent properties of its mechanics.<br>
+<br>
+But comparisons with Tetris aren't very helpful, because Primrose does not have you dropping tiles down a well.  Primrose also doesn't have you swapping adjacent tiles like so many match-3 games.  So what comparison is useful?  Primrose is a small bit like Go and Othello (if either of those were one-player games), a small bit like Lights Out, and a big bit like nothing you've every played before.<br>
+<br>
+<br>
+Primrose includes the following features:
+<ul>
+<li>Live, networked high score boards.  Compete against the world right from your iPhone, wherever you are.
+<br>
+<br>
+
+<li>Instant replay, move-by-move, of all games on the high score boards.  Wondering how other players got those unbelievable scores?  Just touch Play and watch how they did it.
+<br>
+<br>
+
+<li>Comprehensive instruction pages with realtime, animated examples.
+<br>
+<br>
+
+<li>Glowing, pulsating, retro graphics and analog synth-inspired sound effects.
+<br>
+<br>
+
+<li>Behaves like a proper iPhone app by auto-saving game progress during calls and other interruptions.  Take the call, then resume right where you left off after the call ends.
+<br>
+<br>
+
+<li>Colorblind mode available.
+</ul>
+
+Press the [P] button on the main screen to access the menu.<br>
+<br>
+You can read an <a href="http://www.esquire.com/features/best-and-brightest-2008/future-of-video-game-design-1208">in-depth profile</a> of Jason Rohrer in Esquire Magazine's December 2008 "Genius" issue.
+
+<?php  include_once( "foot.html" ); ?>
---
+++ tags/Primrose_v6c/documentation/html/index.php
@@ -0,0 +1,30 @@
+
+<?php  include_once( "head.html" ); ?>
+
+
+<center>
+<a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=302872239&mt=8"><img src="storeBadge.png" width=174 height=58 border=0></a>
+</center>
+
+<br>
+<br>
+<br>
+
+<font size=5><a href="description.php">Description</a></font>
+
+<br>
+<br>
+<br>
+
+<font size=5><a href="screens.php">Screenshots</a></font>
+
+<br>
+<br>
+<br>
+
+<font size=5><a href="download.php">Downloads</a></font><br>
+Mac, Windows, and GNU/Linux
+
+
+
+<?php  include_once( "foot.html" ); ?>
